43540a1574e7eb315ef3f508e42d93d57f877197
urkoalber <urkito91@hotmail.com>
2014-11-19 15:25:50 +0100

    basic request api

--- app.rb
+++ app.rb
@@ -20,6 +20,7 @@ end
 
 class Chat < ActiveRecord::Base
   before_save :default_values
+  has_many :messages
   has_and_belongs_to_many :users
 
   def default_values
@@ -50,6 +51,26 @@ def get_user_chats(id)
   User.find(id).chats || []
 end
 
+def get_user_messages(id)
+  User.find(id).messages || []
+end
+
+def get_all_chats
+  Chat.all || []
+end
+
+def get_chat(id)
+  Chat.find(id)
+end
+
+def get_chat_users(id)
+  Chat.find(id).users || []
+end
+
+def get_chat_messages(id)
+  Chat.find(id).messages || []
+end
+
 before do
   content_type :json, :charset => 'utf-8'
 end
@@ -73,6 +94,46 @@ end
 get '/api/users/:id/chats' do
   if chats = get_user_chats(params[:id])
     chats.to_json
+  else
+    halt 500
+  end  
+end
+
+get '/api/users/:id/messages' do
+  if messages = get_user_messages(params[:id])
+    messages.to_json
+  else
+    halt 500
+  end   
+end
+
+get '/api/chats' do
+  if chats = get_all_chats
+    chats.to_json
+  else
+    halt 500
+  end   
+end
+
+get '/api/chats/:id' do
+  if chat = get_chat(params[:id])
+    chat.to_json
+  else
+    halt 404  
+  end  
+end
+
+get '/api/chats/:id/users' do
+  if users = get_chat_users(params[:id])
+    users.to_json
+  else
+    halt 500
+  end  
+end
+
+get '/api/chats/:id/messages' do
+  if messages = get_chat_messages(params[:id])
+    messages.to_json
   else
     halt 500
   end  
