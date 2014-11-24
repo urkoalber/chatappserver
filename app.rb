e01c30d8eff3739ea675210773de1a21046f2c37
urkoalber <urkito91@hotmail.com>
2014-11-24 02:05:09 +0100

    added some funct

--- app.rb
+++ app.rb
@@ -11,10 +11,15 @@ configure :production do
   ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'] || 'postgres://localhost/chatappserver')
 end
 
+use Rack::Session::Cookie, :expire_after => 2592000, # In seconds
+                           :secret => 'chatappserversuperstrongpassword'
+
 class User < ActiveRecord::Base
   before_save :default_values
   has_and_belongs_to_many :chats
   has_many :messages
+  validates :username, :presence => true
+  validates :pin, :presence => true
   validates :phone, :presence => true, :uniqueness => true
 
   def default_values
@@ -38,6 +43,8 @@ class Message < ActiveRecord::Base
   before_save :default_values
   belongs_to :user
   belongs_to :chat
+  validates :user, :presence => true
+  validates :chat, :presence => true
 
   def default_values
     self.message_type ||= 'text'
@@ -45,6 +52,143 @@ class Message < ActiveRecord::Base
   end  
 end
 
+helpers do
+  def clear_sessions
+    session[:user_id] = nil
+    session[:user_phone] = nil
+  end
+end
+
+before do
+  content_type :json, :charset => 'utf-8'
+  unless request.path_info.split('/').include? 'auth'
+    if session[:user_id] && session[:user_phone]
+      @current_user = User.find_by_id(session[:user_id])
+      unless @current_user && @current_user.phone == session[:user_phone]
+        clear_sessions
+        halt 401
+      end
+    else
+      halt 401
+    end    
+  end
+end
+
+post '/api/auth/login' do
+  req = JSON.parse(request.body.read)
+  user = User.find_by_phone(req['phone'])
+  if user && user.pin == req['pin'].to_i
+    session[:user_id] = user.id
+    session[:user_phone] = user.phone
+    redirect '/api/users/me'
+  else
+    halt 401, {:message => 'Unauthorized'}.to_json
+  end
+end
+
+get '/api/auth/logout' do
+  clear_sessions
+  {:message => 'Correct logout'}.to_json
+end
+
+get '/api/users/me' do
+  if user = @current_user
+    user.to_json
+  else
+    halt 500, { message: "Unexpected error" }.to_json
+  end  
+end
+
+get '/api/users' do
+  redirect '/api/users/me'
+end
+
+put '/api/users/me' do
+  req = JSON.parse(request.body.read)
+  if @current_user.update(req["user"])
+    session[:user_phone] = @current_user.phone
+    redirect "/api/users/me"
+  else
+    halt 400
+  end
+end
+
+post '/api/users' do
+  req = JSON.parse(request.body.read)
+  if user = User.new(req["user"])
+    if user.valid? && user.save
+      session[:user_id] = user.id
+      session[:user_phone] = user.phone
+      redirect "/api/users/me"
+    else
+      halt 409
+    end
+  else
+    halt 400
+  end  
+end
+
+get '/api/messages' do
+  if messages = @current_user.messages
+    messages.to_json
+  else
+    halt 500, { message: "Unexpected error" }.to_json
+  end  
+end
+
+get '/api/chats' do
+  if chats = @current_user.chats
+    chats.to_json
+  else
+    halt 500
+  end    
+end
+
+get '/api/chats/:id' do
+  if chat = @current_user.chats[params[:id].to_i]
+    chat.to_json
+  else
+    halt 500
+  end    
+end
+
+get '/api/chats/:id/messages' do
+  if messages = @current_user.chats[params[:id].to_i].messages
+    messages.to_json
+  else
+    halt 500
+  end      
+end  
+
+post '/api/chats/:id/messages' do
+  req = JSON.parse(request.body.read)
+  chat = @current_user.chats[params[:id].to_i]
+  if message = Message.new(:user => @current_user, :chat => chat, :message_type => req["message"]["message_type"], :content => req["message"]["content"])
+    if message.valid? && message.save
+      redirect "/api/chats/#{params[:id]}/messages"
+    else
+      halt 500
+    end    
+  else
+    halt 400
+  end
+end
+
+# Any other case
+get '/*' do
+  halt 404, { message: "Page not found" }.to_json
+end
+post '/*' do
+  halt 404, { message: "Page not found" }.to_json
+end
+put '/*' do
+  halt 404, { message: "Page not found" }.to_json
+end
+delete '/*' do
+  halt 404, { message: "Page not found" }.to_json
+end
+
+=begin
 def get_all_users
   User.all || []
 end
@@ -77,9 +221,11 @@ def get_chat_messages(id)
   Chat.find(id).messages || []
 end
 
-before do
-  content_type :json, :charset => 'utf-8'
+def get_user_by_phone(phone)
+  User.find_by_phone(phone)
 end
+
+#-------
 
 get '/api/users' do
   if users = get_all_users
@@ -89,11 +235,24 @@ get '/api/users' do
   end 
 end
 
+post '/api/users' do
+  request_body = JSON.parse(request.body.read)
+  if user = User.new(request_body["user"])
+    if user.save
+      redirect "/api/users/#{user.id}"
+    else
+      halt 409
+    end
+  else
+    halt 400
+  end
+end
+
 get '/api/users/:id' do
   if user = get_user(params[:id])
     user.to_json
   else
-    halt 404  
+    halt 404, json({ message: "Not found" })
   end
 end
 
@@ -144,3 +303,12 @@ get '/api/chats/:id/messages' do
     halt 500
   end  
 end
+
+get '/api/phone/:phone' do  
+  if user = get_user_by_phone(params[:phone])
+    user.to_json
+  else
+    halt 404  
+  end    
+end
+=end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141119121026) do
+ActiveRecord::Schema.define(version: 20141123224821) do
 
   create_table "chats", force: true do |t|
     t.string   "name"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20141119121026) do
     t.string   "phone"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "pin"
   end
 
   create_table "users_chats", id: false, force: true do |t|
